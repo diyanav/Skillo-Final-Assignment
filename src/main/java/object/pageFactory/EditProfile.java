678358b71bf3e897df9bebd950289bfb3cda1059
diyanav <diyana.v20@gmail.com>
2023-08-21 00:07:20 +0300

    in-progress

--- src/main/java/object/pageFactory/EditProfile.java
+++ src/main/java/object/pageFactory/EditProfile.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class EditProfile {
     private final WebDriver driver;
@@ -15,6 +17,15 @@ public class EditProfile {
 
     @FindBy(tagName = "h4")
     private WebElement editProfileBoxTitle;
+
+    @FindBy(css = "[formcontrolname='publicInfo']")
+    private WebElement publicInfoField;
+
+    @FindBy(css = "[type=submit]")
+    private WebElement saveButtonEditProfile;
+
+    @FindBy(xpath = "//div[5]/p")
+    private WebElement actualPublicInfo;
 
     public EditProfile(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -25,5 +36,39 @@ public class EditProfile {
     public String getEditProfileBoxTitle() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         return editProfileBoxTitle.getText();
+    }
+
+    public boolean editPublicInfo() {
+        publicInfoField.click();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        String newPublicInfo = getRandomPublicInfo();
+
+        publicInfoField.clear();
+        publicInfoField.sendKeys(newPublicInfo);
+
+        wait.until(ExpectedConditions.elementToBeClickable(saveButtonEditProfile));
+        saveButtonEditProfile.click();
+
+        boolean isPublicInfoUpdated = String.valueOf(actualPublicInfo).contains(newPublicInfo);
+
+        return isPublicInfoUpdated;
+    }
+
+    public String getRandomPublicInfo() {
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 6;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+
+        String randomPublicInfo = buffer.toString();
+
+        return randomPublicInfo;
     }
 }
